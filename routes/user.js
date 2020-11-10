0ecfa5fd94e175a70731683b5ae9740e693c708e
GossipDolphin <mmollevik@hotmail.com>
2020-11-10 14:33:00 +0100

    first commit

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import {userController} from '../controller/index.js'
+
+const router = express.Router();
+
+router.get('/:id', userController.get);
+
+router.get('/', userController.list);
+
+router.post('/create', userController.create);
+
+router.put('/update/:id', userController.update);
+
+router.delete('/delete/:id', userController.remove);
+
+export default router;
